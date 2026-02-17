749452246c3d50273d809ed54e8befb1878f0b4c
siweihuang <siwei.io.huang@gmail.com>
2026-02-17 17:38:27 +0800

    Added P2 Chinese

--- Education/ChineseSpelling/data/mainList_2026.js
+++ Education/ChineseSpelling/data/mainList_2026.js
@@ -6,6 +6,12 @@ const mainList = [
     filename: "AY2026_P2_ChineseSpelling_01"
   },
   {
+    title: "P2 第三课，(HsZ)",
+    subtitle: "第三课",
+    testdate: "",
+    filename: "AY2026_P2_ChineseSpelling_03"
+  },
+  {
     title: "P5 第四课，(HsH)",
     subtitle: "第四课",
     testdate: "2026-02-25 (星期二)",
