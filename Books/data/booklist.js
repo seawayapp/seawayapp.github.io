eefc4986ac50f6870c8598416ccf475d90ff64d3
siweihuang <siwei.io.huang@gmail.com>
2026-04-22 00:07:39 +0800

    Added a new book

--- Books/data/booklist.js
+++ Books/data/booklist.js
@@ -16,5 +16,11 @@ var mainData = [
     subtitle: "大学 / 中庸 / 论语 / 孟子",
     category: "folder",
     filename: "The_Four_Books/index.html"
+  },
+  {
+    title: "The Children Who Loved Books 爱书的孩子",
+    subtitle: "彼得·卡纳沃斯 [加]",
+    category: "folder",
+    filename: "The_Children_Who_Loved_Books/index.html"
   }
 ];
