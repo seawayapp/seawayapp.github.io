1548349cf69725987365bba1eb6861cb1823c9e6
msnbdd <seawayapp@gmail.com>
2026-05-18 16:54:36 +0800

    Update RN

--- ReadingNotes/data/A_index_Reading_Notes.js
+++ ReadingNotes/data/A_index_Reading_Notes.js
@@ -4,34 +4,48 @@ var mainData = [
     subtitle: "Part 1",
     category: "file_slide",
     filename: "RN_Ind_365_Part_1",
-    foldername: "",
+    foldername: ""
   },
   {
     title: "Focus on Yourself & Stay Silent",
     subtitle: "专注自己，保持沉默",
     category: "file_slide",
     filename: "RN_Focus_on_Yourself",
-    foldername: "",
+    foldername: ""
   },
   {
     title: "What It Takes",
     subtitle: "成功之道：追求卓越的经验之谈",
     category: "file_slide",
     filename: "RN_What_It_Takes",
-    foldername: "",
+    foldername: ""
   },
   {
     title: "A Philosopher's Last Lesson",
     subtitle: "哲学家的最后一课",
     category: "file_slide",
     filename: "RN_A_Philosopher_Last_Lesson",
-    foldername: "",
+    foldername: ""
   },
   {
-    title: "The Power Of Now",
+    title: "The Power Of Now (Slide)",
     subtitle: "当下的力量",
     category: "file_slide",
     filename: "RN_The_Power_Of_Now",
-    foldername: "",
+    foldername: ""
   },
+  {
+    title: "The Power Of Now (Table)",
+    subtitle: "当下的力量",
+    category: "file_bilingual",
+    filename: "RN_The_Power_Of_Now",
+    foldername: ""
+  },
+  {
+    title: "How to Talk So Kids Will Listen (Slide)",
+    subtitle: "如何说孩子才会听，怎么听孩子才肯说",
+    category: "file_slide",
+    filename: "RN_How_To_Talk",
+    foldername: ""
+  }
 ];
--- ReadingNotes/data/RN_The_Power_Of_Now.js
+++ ReadingNotes/data/RN_The_Power_Of_Now.js
@@ -3,52 +3,148 @@ var mainData = {
   subtitle: "当下的力量",
   paragraph: [
     {
-      en: "True salvation is a state of freedom -- from fear, from suffering, from a perceived state of lack and insufficiency and therefore from all wanting, needing, grasping, and clinging.  It is freedom from compulsive thinking, from negativity, and above all from past and future as a psychological need. <span class='hl2'>Your mind is telling you that you cannot get there from here.</span>  <span class='hl3'>Something needs to happen</span>, or you need to become this or that before you can be free and fulfilled.",
+      en: "This means that it is accessible to you now as your own deepest self, your true nature. But don't seek to grasp it with your mind. Don't try to understand it. You can know it only when the mind is still. When you are present, when your attention is fully and intensely in the Now, Being can be felt, but it can never be understood mentally. To regain awareness of Being and to abide in that state of “<span class='hl3'>feeling-realization</span>” is enlightenment.",
+      cn: "也就是说，作为你最深的自我和真实的本质，你可以在每个当下接触到它。别试着去掌握它的含义，别试着去理解它。只有当你的思维处于静止时，你才会领会它的真正含义。当你的思维处于静止时，当你的注意力完全集中在当下时刻时，你就会感觉到本体，但是从心智上我们永远无法理解它。重新觉知到本体，并保持这种<span class='hl3'>觉知体验</span>的状态就是开悟。",
+    },
+    {
+      en: "The word Being explains nothing, but nor does God. Being, however, has the advantage that it is an open concept. It does not reduce the infinite invisible to a finite entity. It is impossible to form a mental image of it. Nobody can claim exclusive possession of Being. It is your very essence, and it is immediately accessible to you as the feeling of your own presence, the realization I am that is prior to I am this or I am that. So it is only a small step from the word Being to the experience of Being.",
+      cn: "和上帝一样，“本体”这个词也没有解释任何东西。然而，本体是一个开放的概念，这是它的优势。它没有将一个无限的、无形的东西缩减成一个有限的实体。人们不可能在头脑中构想出一个关于本体的意象，也没有人能够宣称他独自拥有本体。它是你的本质，只有当你感觉到临在时，你才会立即领会到“我是”的真谛。所以从“本体”这个词到体验到你的“本体”，只是小小的一步而已。",
+    },
+    {
+      en: "Not to be able to stop thinking is a dreadful affliction, but we don't realize this because almost everybody is suffering from it, so it is considered normal. This incessant mental noise prevents you from finding that realm of inner stillness that is inseparable from Being. It also creates a false mind-made self that casts a shadow of fear and suffering. ",
+      cn: "不能停止思考是一个可怕的烦恼，由于几乎每一个人都遭受着此种痛苦，而我们又无法意识到这一点，所以这就成了一件很正常的事情。这种不停的思维活动使你无法达到内心的宁静状态。同时，它创造了一个虚假的自我，不断投射出恐惧和苦难的阴影。",
+    },
+    {
+      en: "The voice comments, speculates, judges, compares, complains, likes, dislikes, and so on. The voice isn't necessarily relevant to the situation you find yourself in at the time; it may be reviving the recent or distant past or rehearsing or imagining possible future situations. Here it often imagines things going wrong and negative outcomes; this is called worry.",
+      cn: "那个声音不停地在评论、推测、批判、比较、抱怨、选择好恶等。这种声音可能与你当下所在的情况无关，它可能是关于过去或未来的一些事情，它可能是在回忆往昔，或是在幻想未来可能发生的事情。它经常想象事情可能会出差错，或产生不利的后果，这就是杞人忧天。",
+    },
+    {
+      en: "So you see and judge the present through the eyes of the past and get a totally distorted view of it. It is not uncommon for the voice to be a person's own worst enemy. Many people live with a tormentor in their head that continuously attacks and punishes them and drains them of vital energy. It is the cause of untold misery and unhappiness, as well as of disease.",
+      cn: "所以，你以对过去的看法来判断现在的事情，一定会得到一个完全被歪曲的理解。这个声音是人类自己最大的敌人，这是毫不夸张的。许多人在大脑的折磨下度过一生，任由它攻击、惩罚，并耗尽生命的能量。这就是数不清的灾难、痛苦以及疾病产生的原因。",
+    },
+    {
+      en: "Instead of “watching the thinker”, you can also create a gap in the mind stream simply by directing the focus of your attention into the Now. Just become intensely conscious of the present moment. This is a deeply satisfying thing to do. In this way, you draw consciousness away from mind activity and create a gap of no-mind in which you are highly alert and aware but not thinking. This is the essence of meditation.",
+      cn: "除了“观察思考者”之外，你还可以通过将你的注意力集中在当下这一刻，从而在思维中创造那种空白。就是全神贯注于当下的时刻。这是一件非常值得去做的事情。通过这种方式，你可以将意识从思维活动中引开，并创造一种无思维的空白。在这种空白中，你高度警惕，注意力高度集中，但是你没有在思考。这就是冥想的本质。",
+    },
+    {
+      en: "There is one certain criterion by which you can measure your success in this practice: the degree of peace that you feel within.",
+      cn: "有一个标准可以用来衡量你的练习是否成功：你内在所感觉到的平和的程度。",
+    },
+    {
+      en: "learn to disidentify from your mind. Every time you create a gap in the stream of mind, the light of your consciousness grows stronger. One day you may catch yourself smiling at the voice in your head, as you would smile at the antics of a child. This means that you no longer take the content of your mind all that seriously, as your sense of self does not depend on it.",
+      cn: "从对思维的认同中摆脱出来。每次，当你在思维中创造空白时，你的意识就会变得更强。某天，你可能会发现你在冲着你大脑中的声音微笑，就像你冲着孩子调皮的动作微笑一样。这意味着你不再认真地对待你思维的内容，因为你的自我意识不再依赖于它。",
+    },
+    {
+      en: "Your mind is an instrument, a tool. It is there to be used for a specific task, and when the task is completed, you lay it down.",
+      cn: "你的大脑只是一个工具。它是被用来处理特殊任务的，当这个任务完成时，你就让它处于休止状态。",
+    },
+    {
+      en: "As you grow up, you form a mental image of who you are, based on your personal and cultural conditioning. We may call this phantom self the ego. It consists of mind activity and can only be kept going through constant thinking. ",
+      cn: "随着你逐渐长大，在你个人和文化环境的制约下，你在脑海中勾勒出了自己的形象。我们不妨把这个虚幻的自我称为“小我”。小我由思维活动组成，只有不断地进行思考它才能存活。",
+    },
+    {
+      en: "To the ego, the present moment hardly exists. Only past and future are considered important. This total reversal of the truth accounts for the fact that in the ego mode the mind is so dysfunctional. It is always concerned with keeping the past alive, because without it - who are you? It constantly projects itself into the future to ensure its continued survival and to seek some kind of release or fulfilment there. ",
+      cn: "对于小我来说，当下时刻几乎不存在，只有过去和未来才是重要的——完全颠倒是非。这是因为，当我们的大脑由小我掌控时，它是功能失调的。小我尽力使过去发生的事情复活，因为如果没有过去，你将会是谁？它还不断地把自己投射到未来，以确保它能继续存活，并且在未来寻找某种慰藉或满足。",
+    },
+    {
+      en: "It says: “One day, when this, that, or the other happens, I am going to be okay, happy, at peace.”",
+      cn: "它会说：“某天，当这个、那个或其他的事情发生时，我就会很好、很幸福，也很平和。”",
+    },
+    {
+      en: "Thinking and consciousness are not synonymous. Thinking is only a small aspect of consciousness. Thought cannot exist without consciousness, but consciousness does not need thought.",
+      cn: "思维和意识不是同义词，思维只是意识的一小部分。如果没有意识，思维将不复存在，但是意识的存在不需要思维。",
+    },
+    {
+      en: "Emotion: The Body's Reaction to Your Mind",
+      cn: "情绪：身体对思维的反应",
+    },
+    {
+      en: "What about emotions? I get caught up in my emotions more than I do in my mind.",
+      cn: "情绪是什么？我陷入情绪的时间比陷入思维的时间多。",
+    },
+    {
+      en: "Emotion arises at the place where mind and body meet. It is the body's reaction to your mind - or you might say, a reflection of your mind in the body.",
+      cn: "情绪在思维和身体的相遇处产生。它是身体对思维的反应——或者可以说，它是思维在身体上的反映。",
+    },
+    {
+      en: "Research has shown that strong emotions even cause changes in the biochemistry of the body. These biochemical changes represent the physical or material aspect of the emotion. ",
+      cn: "研究显示，强烈的情感甚至会导致身体的生理变化。这些生理变化代表着情绪的身体层面或物质层面。",
+    },
+    {
+      en: "The more you are identified with your thinking, your likes and dislikes, judgments and interpretations, which is to say the less present you are as the watching consciousness, the stronger the emotional energy charge will be, whether you are aware of it or not.",
+      cn: "你被你的思维、喜好、判断以及分析控制得越多，也就是说你的观察者意识越少的话，你的情绪能量的负荷就会越强，不管你是否意识到了这一点。",
+    },
+    {
+      en: "If you cannot feel your emotions, if you are cut off from them, you will eventually experience them on a purely physical level, as a physical problem or symptom. ",
+      cn: "如果你不能感受到你的情绪，或是切断了与情绪的联系，那么你最终会在纯生理这一层面体验到它们，它们会以生理问题或疾病的形式出现。",
+    },
+    {
+      en: "For example, I have observed that <span class='hl2'>people who carry a lot of anger inside without being aware of it and without expressing it</span> are more likely to be attacked, verbally or even physically, by other angry people, and often for no apparent reason. They have a strong emanation of anger that certain people pick up subliminally and that triggers their own latent anger.",
+      cn: "比如，我观察到那些内心充满愤怒而没有意识到这一点，也没有表达愤怒的人，更容易从言语上或身体上遭受其他愤怒之人的莫名其妙的攻击。这是因为他们散发着强烈的愤怒波动，被某人无意识地接收，从而引爆了这个人自己内心潜伏的愤怒。",
+    },
+    {
+      en: "If you really want to know your mind, <span class='hl2'>the body will always give you a truthful reflection</span>, so look at the emotion or rather feel it in your body. If there is an apparent conflict between them, <span class='hl2'>the thought will be the lie, the emotion will be the truth</span>. Not the ultimate truth of who you are, but the <span class='hl3'>relative truth of your state of mind at that time</span>.",
+      cn: "如果你真的想了解你的思维，<span class='hl2'>身体总是会给你一个真实的反映</span>，所以请在你体内去看或是感受它。如果两者之间有明显的分歧，<span class='hl2'>那么思维永远是说谎的一方，情绪则始终是真实的</span>。这里所说的真实，并不是指你是谁的终极真理，而是你<span class='hl3'>当时思维状态的相对真实</span>。",
+    },
+    {
+      en: "While a thought is in your head, an emotion has a strong physical component and so is primarily felt in the body. You can then allow the emotion to be there without being controlled by it. You no longer are the emotion; you are the watcher, the observing presence. If you practice this, all that is unconscious in you will be brought into the light of consciousness.",
+      cn: "思维存在于你的大脑里，情感却具有强烈的生理成分，所以你可以在体内感觉到它。然后，你可允许情绪的存在，但却不要受它控制。你不再是你的情绪，而是一个观察者。如果你这样做，所有无意识的东西都将会被意识之光所照亮。",
+    },
+    {
+      en: "So observing our emotions is as important as observing our thoughts? Yes. Make it a habit to ask yourself: What's going on inside me at this moment? That question will point you in the right direction. But don't analyze, just watch. Focus your attention within. Feel the energy of the emotion. If there is no emotion present, take your attention more deeply into the inner energy field of your body. It is the doorway into Being.",
+      cn: "所以，观察我们的情绪像观察我们的思维一样重要吗？是的。你应该习惯这样问自己：“此刻，我内在发生了什么事情？”这个问题将会把你引向正确的方向。但是，请不要进行分析，观察就可以了。请将你的注意力集中于内在，并且去感觉情绪的能量。如果没有情绪存在，那么请更深地去关注你身体的内在能量场。这就是进入本体的大门。",
+    },
+    {
+      en: "An emotion usually represents an amplified and energized thought pattern, and because of its often overpowering energetic charge, it is not easy initially to stay present enough to be able to watch it.  It wants to take you over, and it usually succeeds - unless there is enough presence in you. ",
+      cn: "情绪通常代表着一个被强化的以及被注入能量的思维模式，由于它的能量是蓄势待发的，所以一开始我们不容易维持临在的意识而观察到它。它要控制你，而且它通常都会得逞——除非你有足够的临在意识。",
+    },
+    {
+      en: "By dwelling mentally on the situation, event, or person that is the perceived cause of the emotion, the thought feeds energy to the emotion, which in turn energizes the thought pattern, and so on.",
+      cn: "通过在脑海中不断回想那个被视为情绪诱因的情境、事件或人物，思想便源源不断地为情绪提供能量；而情绪反过来又会强化这种思维模式，如此循环往复。",
+    },
+    {
+      en: '"Fear" comes close, but apart from a continuous sense of threat, it also includes a deep sense of abandonment and incompleteness.  It may be best to use a term that is as undifferentiated as that basic emotion and simply call it "pain."',
+      cn: "恐惧可能是最贴近的一个答案，但是除了不断地感到威胁之外，它还包括一种深深的被遗弃感和不完整感。我们把它称为“痛苦”是很合适的，因为它和前面说的那种最基本的情绪一样，是无差别的。",
+    },
+    {
+      en: 'One of the main tasks of the mind is to fight or remove that emotional pain, which is one of the reasons for its incessant activity, but all it can ever achieve is to cover it up temporarily.  In fact, the harder the mind struggles to get rid of the pain, the greater the pain.  The mind can never find the solution, nor can it afford to allow you to find the solution, because it is itself an intrinsic part of the "problem."',
+      cn: "思维的一个主要任务就是反抗或消除这种情绪上的痛苦，这也是思维活动如此之多的原因。然而思维顶多也只能暂时地将痛苦掩盖住。实际上，思维越是努力去摆脱这种痛苦，痛苦就会越深。思维永远不会找到解决方案，它也不会让你找到解决方案，因为它本身就是问题的一部分。",
+    },
+    {
+      en: "True salvation is a state of freedom -- from fear, from suffering, from a perceived state of lack and insufficiency and therefore from all wanting, needing, grasping, and clinging. It is freedom from compulsive thinking, from negativity, and above all from past and future as a psychological need. <span class='hl2'>Your mind is telling you that you cannot get there from here.</span> <span class='hl3'>Something needs to happen</span>, or you need to become this or that before you can be free and fulfilled.",
       cn: "真正的拯救是一种自由状态——从恐惧、痛苦、匮乏和不满的感觉中解脱，从所有的欲望需求、占据和依赖中解放出来。它是从强迫性思维、消极心态，最重要的是，从以心理需求形成的过去和未来中解脱。你的思维不断告诉你，你不能从这里到那里。必须有些事先发生了，<span class='hl2'>或是你必经成为这个或那个，你才可能被解放或得到满足。</span>",
     },
     {
-      en: "In fact, the moment that judgment stops through acceptance of what is, you are free of the mind.  You have made room for love, for joy, for peace.  First you stop judging yourself then you stop judging your partner.  The greatest catalyst for change in a relationship is complete acceptance of your partner as he or she is, without needing to judge or change them in any way.  That immediately takes you beyond ego.  All mind games and all addictive clinging are then over.  There are no victims and no perpetrators anymore, no accuser and accused, ",
+      en: "In fact, the moment that judgment stops through acceptance of what is, you are free of the mind. You have made room for love, for joy, for peace. First you stop judging yourself then you stop judging your partner. The greatest catalyst for change in a relationship is complete acceptance of your partner as he or she is, without needing to judge or change them in any way. That immediately takes you beyond ego. All mind games and all addictive clinging are then over. There are no victims and no perpetrators anymore, no accuser and accused, ",
       cn: "事实上，当你通过接受事实而让批判停止时，你就从你的思维中解放出来了。你就已经为爱、喜悦、和平创造了空间。首先，停止批判你自己，然后，停止批判你的伴侣。爱情最伟大的催化剂就是完全接受你伴侣的一切，而不是去批判或以任何方式去改变他或她。这样，你就立即超越了小我。所有的思维游戏和沉溺依赖都将消失。再也没有受害者和加害者，也没有原告和被告。",
     },
     {
-      en: "Love is a state of Being.  Your love is not outside; it is deep within you.  You can never lose it, and it cannot leave you.  It is not dependent on some other body, some external form.",
+      en: "Love is a state of Being. Your love is not outside; it is deep within you. You can never lose it, and it cannot leave you. It is not dependent on some other body, some external form.",
       cn: "爱是一种本体的状态。你的爱不在你身外，你的爱在你之内。你永远不会失去它，它也不会离开你。爱不需要依赖一个人、一个外在的形式才能存在。",
     },
     {
-      en: "",
+      en: "How many people does it take to make your life into a spiritual practice? Never mind if your partner will not cooperate. Sanity -- consciousness -- can only come into this world through you. You do not need to wait for the world to become sane, or for somebody else to become conscious, before you can be enlightened. You may wait forever.",
+      cn: "你需要多少人才能把你的生活变成灵修的实践呢？如果你的伴侣不合作，请不要介意。明智，即意识，只有通过你才能来到这个世界上。你的开悟不需要去等待这个世界变得明智，或别人变得有意识。",
+    },
+    {
+      en: 'It is not easy to live with an enlightened person, or rather it is so easy that the ego finds it extremely threatening. Remember that the ego needs problems, conflict, and "enemies" to strengthen the sense of separateness on which its identity depends. The unenlightened partner\'s mind will be deeply frustrated because its fixed positions are not resisted, which means they will become shaky and weak, and there is even the "danger" that they may collapse altogether, resulting in loss of self The pain-body is demanding feedback and not getting it.',
+      cn: "与一个开悟的人相处并不容易，小我会很容易地发现它面临巨大的威胁。记住，小我需要问题、冲突和“敌人”来强化它身份赖以生存的孤立感。跟开悟的伴侣在一起，未开悟的那一方的思维会深深受挫，因为没有东西来抵抗它们，也就是说它们会变得脆弱，并且还有全部瓦解的危险，从而导致了小我的丧失。痛苦之身需要反馈，但是却又得不到。",
+    },
+    {
+      en: 'Another indicator is how a person behaves in difficult or challenging situations or when things "go wrong." If your "enlightenment" is egoic self-delusion, then life will soon give you a challenge that will bring out your unconsciousness in whatever form -- as fear, anger, defensiveness, judgment, depression, and so on.',
+      cn: "判断是否开悟的另外一个方法是，看一个人在困难或充满挑战的环境中或当事情出错时如何行事。如果你的开悟是小我的自我幻象，那么你的生命很快就会为你带来一些挑战，这些挑战将会让你的无意识以任何一种形式的痛苦展现出来，如恐惧、愤怒、防御、批判、抑郁等。",
+    },
+    {
+      en: "The first thing to remember is this: As long as you make an identity for yourself out of the pain, you cannot become free of it.",
+      cn: "第一件需要记住的事是：只要你从痛苦中汲取你的身份认同，你就无法从痛苦中解放出来。",
+    },
+    {
+      en: "Of course, the pain-body is there because of certain things that happened in the past.",
       cn: "",
     },
     {
       en: "",
-      cn: "",
-    },
-    {
-      en: "",
-      cn: "",
-    },
-    {
-      en: "",
-      cn: "",
-    },
-    {
-      en: "",
-      cn: "",
-    },
-    {
-      en: "",
-      cn: "",
-    },
-    {
-      en: "",
-      cn: "",
-    },
-    {
-      en: "",
-      cn: "",
-    },
-    {
-      en: "",
-      cn: "",
+      cn: "当然，痛苦之身之所以存在是因为过去发生的事情。它是你活生生的过去，如果你对它认同，就是对过去认同。",
     },
   ],
 };
