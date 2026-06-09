411364d512eea47ca8caa830ee4004e75d3c988c
siweihuang <siwei.io.huang@gmail.com>
2026-06-10 00:47:25 +0800

    Updated for 20260613

---
+++ Education/PSLE/EL/data/A_index_sentence.js
@@ -0,0 +1,9 @@
+var mainData = [
+  {
+    title: "Nanyang Primary EL Paper 2",
+    subtitle: "2024_P5_EOY",
+    category: "file_slide",
+    filename: "P5_EOY_Sentences_1",
+    foldername: ""
+  }
+];
---
+++ Education/PSLE/EL/data/P5_EOY_Sentences_1.js
@@ -0,0 +1,58 @@
+var mainData = {
+  title: "Nanyang Primary EL Paper 2",
+  subtitle: "2024_P5_EOY",
+  paragraph: [
+    {
+      en: "The cashier will ask you to complete a form should you change your mind and ask for a refund for your purchase.",
+      cn: ""
+    },
+    {
+      en: "Neither the boys nor their mother is keen to go to the theme park during the upcoming holidays.",
+      cn: ""
+    },
+    {
+      en: "Lakeside Park has a/an tranquil environment suitable for anyone looking for a quiet spot to relax after a hard day's work.",
+      cn: ""
+    },
+    {
+      en: "\"We may have lost to the other school in the first round, but there's a chance we might still make it to the finals. Let's tighten our belts and stay in the game!\" Bob exclaimed.",
+      cn: ""
+    },
+    {
+      en: "Amid the hustle and bustle of city life, Little Islands attracts its viewers to stay a while longer.",
+      cn: ""
+    },
+    {
+      en: "The addition of art into the community is indeed seamlessly done.",
+      cn: ""
+    },
+    {
+      en: "We are the dreammakers, crafting your favourite toys that light up the world with excitement and joy!",
+      cn: ""
+    },
+    {
+      en: "What truly makes Happy Toys unique is our collection of toys.",
+      cn: ""
+    },
+    {
+      en: "Each toy we craft holds a tale, waiting eagerly to unravel in a child's hands on their most special day.",
+      cn: ""
+    },
+    {
+      en: "Our toys are made from recyclable materials, as we consistently seek new ways to be kinder to our planet.",
+      cn: ""
+    },
+    {
+      en: "What might typically take ten minutes to clean often stretches to an hour due to such messes, making cleanning an overwhelming task.",
+      cn: ""
+    },
+    {
+      en: "",
+      cn: ""
+    },
+    {
+      en: "",
+      cn: ""
+    }
+  ]
+};
