3303789e6638abc34120d970b99c313f12968c40
msnbdd <seawayapp@gmail.com>
2026-07-01 06:48:15 +0800

    Updated P5 Chinese Spelling 10

---
+++ Education/ChineseSpelling/data/AY2026_P5_ChineseSpelling_10.js
@@ -0,0 +1,84 @@
+const mainData = [
+  {
+    category: "华文听写 2026",
+    title: "第十课，分享是快乐的 (HSH)",
+    subtitle: "第十课",
+    testdate: "7月1日",
+    group: [
+      "识写字词",
+      "识写字词",
+      "识写字词",
+      "识写字词",
+      "识写字词",
+      "识写字词",
+      "识写字词",
+      "识写字词",
+      "识写字词",
+      "识写字词",
+
+      "词语搭配",
+      "词语搭配",
+      "词语搭配",
+      "词语搭配",
+      "词语搭配",
+      "词语搭配",
+      "词语搭配",
+      "词语搭配",
+
+      "好句子",
+      "好句子",
+
+      "默写"
+    ],
+    sentences: [
+      "比较",
+      "一段",
+      "丰盛",
+      "刷子",
+      "弯腰",
+      "农夫",
+      "惭愧",
+      "珍惜",
+      "拥有",
+
+      "热线服务",
+      "网络平台",
+      "电邮地址",
+      "嘲笑别人",
+      "隐形眼镜",
+      "无法控制",
+      "唠叨不停",
+      "遮住饭盒",
+
+      "妈妈准备了各式各样的食物，包括炒面、蛋糕、饮料…来招待客人。",
+      "眼看着火了，我惊吓得脸色苍白，不知所措。弟弟更是吓得号啕大哭起来。",
+
+      "老师说：“同学之间应该互相爱护，和睦共处，不应该为一件小事而大动干戈。”听了老师的话，他们都知道自己太冲动了。自责和悔恨交织在一起，他们羞得无地自容，恨不得地上有一个洞让他们钻进去躲。他们向对方道歉后，就和好了。"
+    ],
+    pronunciation: [
+      "bǐ jiào",
+      "yī duàn",
+      "fēng shèng",
+      "shuā zi",
+      "wān yāo",
+      "nóng fū",
+      "cán kuì",
+      "zhēn xī",
+      "yōng yǒu",
+
+      "rè xiàn fú wù",
+      "wǎng luò píng tái",
+      "diàn yóu dì zhǐ",
+      "cháo xiào bié rén",
+      "yǐn xíng yǎn jìng",
+      "wú fǎ kòng zhì",
+      "láo dāo bù tíng",
+      "zhē zhù fàn hé",
+
+      "mā ma zhǔn bèi le gè shì gè yàng de shí wù ， bāo kuò chǎo miàn 、 dàn gāo 、 yǐn liào …… lái zhāo dài kè ren 。",
+      "yǎn kàn zhe huǒ le ， wǒ jīng xià de liǎn sè cāng bái ， bù zhī suǒ cuò 。 dì di gèng shì xià de háo táo dà kū qǐ lái 。",
+
+      "lǎo shī shuō ： “ tóng xué zhī jiān yīng gāi hù xiāng ài hù ， hé mù gòng chǔ ， bù yīng gāi wèi yī jiàn xiǎo shì ér dà dòng gān gē 。 ” tīng le lǎo shī de huà ， tā men dōu zhī dào zì jǐ tài chōng dòng le 。 zì zé hé huǐ hèn jiāo zhī zài yī qǐ ， tā men xiū de wú dì zì róng ， hèn bu de dì shàng yǒu yī gè dòng ràng tā men zuān jìn qù duǒ 。 tā men xiàng duì fāng dào qiàn hòu ， jiù hé hǎo le 。"
+    ]
+  }
+];
